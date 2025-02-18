025e9d49415bb0f29c70c652bb5e7572314eee0d
Gareleon <dragan.ignjatovic.student@gmail.com>
2025-02-18 19:46:04 +0100

    fix: TypeError during build phase

--- components/ErrorWindow.tsx
+++ components/ErrorWindow.tsx
@@ -1,4 +1,4 @@
-export default function ErrorWindow({ error }: { error: any }) {
+export default function ErrorWindow({ error }: { error: Error }) {
   return (
     <>
       <p className="font-bold text-lg text-red-600 mb-2">ERROR</p>

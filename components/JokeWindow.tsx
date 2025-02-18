9f8b0923699847ab82ecdf96e5b5f07f82909de5
Gareleon <dragan.ignjatovic.student@gmail.com>
2025-02-18 19:11:17 +0100

    feat: completed JokeWindow component, it shows jokes

--- components/JokeWindow.tsx
+++ components/JokeWindow.tsx
@@ -1,4 +1,5 @@
 import { Joke } from "@/types/data.types";
+import { Button } from "./ui/button";
 
 interface JokeWindowProps {
   localJoke: Joke | null;
@@ -6,5 +7,31 @@ interface JokeWindowProps {
 }
 
 export default function JokeWindow({ localJoke, handleVote }: JokeWindowProps) {
-  return <div>Joke Window</div>;
+  return (
+    <>
+      <p className="font-bold text-lg md:text-xl lg:text-2xl text-gray-800 mb-2 break-words">
+        {localJoke?.question}
+      </p>
+      <p className="text-gray-600 break-words md:text-lg lg:text-xl text-center">
+        {localJoke?.answer}
+      </p>
+      <div className="pt-6 flex flex-wrap gap-2 justify-center items-center">
+        {localJoke?.availableVotes.map((vote: string) => {
+          const voteObject = localJoke.votes.find((v) => v.label === vote);
+          const voteValue = voteObject ? voteObject.value : 0;
+
+          return (
+            <Button
+              key={vote}
+              onClick={() => handleVote(localJoke._id, vote)}
+              className="flex gap-2 items-center"
+            >
+              <span className="text-md md:text-lg font-bold">{voteValue}</span>
+              <span>{vote}</span>
+            </Button>
+          );
+        })}
+      </div>
+    </>
+  );
 }
